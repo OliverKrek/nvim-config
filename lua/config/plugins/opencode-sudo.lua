9d3de38f528af815bb17cbf65c782c979dab7717
Oliver Krek <oliver.krek@gmail.com>
2026-03-03 22:54:59 +0100

    Added support for opencode.

---
+++ lua/config/plugins/opencode-sudo.lua
@@ -0,0 +1,20 @@
+return {
+  "sudo-tee/opencode.nvim",
+  lazy = false,
+  config = function()
+    require("opencode").setup({})
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        anti_conceal = { enabled = false },
+        file_types = { 'markdown', 'opencode_output' },
+      },
+      ft = { 'markdown', 'Avante', 'copilot-chat', 'opencode_output' },
+    },
+    'saghen/blink.cmp',
+    'nvim-telescope/telescope.nvim',
+  },
+}
